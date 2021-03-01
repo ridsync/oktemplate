acb69b6200d0f0429c9d562a52d10c017621d0cd
GitHub Action <action@github.com>
2021-03-01 06:23:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/ridsync/oktemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ridsync/oktemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ridsync.oktemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ridsync.oktemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/ridsync/oktemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ridsync/oktemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ridsync.oktemplate.services
 
+import com.github.ridsync.oktemplate.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
